1b9b9df898699b6a8e31c6024e9ba22de31f9dcd
Kanelee1995 <kaneleepakhung@gmail.com>
2022-12-02 17:53:40 -0500

    added html files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
+const url = require('url');
 const http = require('http');
+const myURL = new URL('https://localhost:8080');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 8080;
 
 const server = http.createServer((req:any, res:any) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    // if (myURL.href === 'localhost:8080') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     res.end('This is index');
+    // } else if (myURL.pathname === '/about') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     res.end('This is about');
+    // } else if (myURL.pathname === '/contact-me') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     res.end('This is contant');
+    // } 
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('This is index');
+    console.log(res);
 });
 
 server.listen(port, hostname, () => {
